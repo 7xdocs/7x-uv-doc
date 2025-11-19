1207b82af9ba2a7f1db33fc156766b6d21820232
Chris <chris@topiaco.com>
2025-11-19 14:36:29 +0800

    docs: init

---
+++ _7x/zh/scripts/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Prepare for a release
+#
+# All additional options are passed to `rooster`
+set -eu
+
+script_root="$(realpath "$(dirname "$0")")"
+project_root="$(dirname "$script_root")"
+
+echo "Updating metadata with rooster..."
+cd "$project_root"
+
+# Update the changelog
+uvx --python 3.12 rooster@0.1.1 release "$@"
+
+echo "Updating lockfile..."
+cargo update -p uv
